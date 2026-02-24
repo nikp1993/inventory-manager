0e9ee42ffcf48687614ebd3326358f544c54c816
PandeyBhai <pandeynikhil1994@gmail.com>
2026-02-24 23:29:25 +0530

    Reusable workflow and composite actions update to include provision, build, test and publish steps

--- infra/terraform/modules/aws/bootstrap/main.tf
+++ infra/terraform/modules/aws/bootstrap/main.tf
@@ -20,3 +20,46 @@ module "tfstate_storage" {
 output "tfstate_aws_bucket_name" {
   value = module.tfstate_storage.bucket_name
 }
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+}
+
+data "aws_iam_policy_document" "github_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:nikp1993/inventory-manager:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name               = "github-actions-terrraform-role"
+  assume_role_policy = data.aws_iam_policy_document.github_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_admin" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "aws_github_role_arn" {
+  value = aws_iam_role.github_actions_role.arn
+}
